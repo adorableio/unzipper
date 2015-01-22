39f6c341779d1853248dbd0f46b3cdcc41d55115
Zachery Moneypenny <whazzmaster@gmail.com>
2015-01-22 11:56:42 -0600

    Updates structure of client

--- lib/unzipper/client.rb
+++ lib/unzipper/client.rb
@@ -2,15 +2,15 @@ module Unzipper
   class Client
     include HTTMultiParty
 
-    def send_zip(file, options)
-      options.validate
+    class << self
+      def send_zip(file, options)
+        options.validate
 
-      response = self.class.post options[:unzipper_url], body: {
-        archive: file,
-        metadata: options.to_multipart_form_data
-      }
-
-      JSON.parse(response.body)
+        post options[:unzipper_url], body: {
+          archive: file,
+          metadata: options.to_multipart_form_data.to_json
+        }
+      end
     end
   end
 end
